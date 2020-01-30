825e2143f25a4d207f43f7678c278c72d325eba0
Dominik Swiatek <dominik.swiatek1995@gmail.com>
2020-01-30 15:13:40 +0100

    Updates

---
+++ precache-manifest.a6134411e66961794a82d33134d4483d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d999a8a0898c2c9bb54527c81d7e12f3",
+    "url": "/portal/index.html"
+  },
+  {
+    "revision": "6d46d5b264340bc0bc4d",
+    "url": "/portal/static/css/main.ce5953a5.chunk.css"
+  },
+  {
+    "revision": "d090d63700f67567bfcd",
+    "url": "/portal/static/js/2.e9dd74b9.chunk.js"
+  },
+  {
+    "revision": "6d46d5b264340bc0bc4d",
+    "url": "/portal/static/js/main.7db166f3.chunk.js"
+  },
+  {
+    "revision": "05d4c05c31e6e52e5e67",
+    "url": "/portal/static/js/runtime~main.3c49a560.js"
+  },
+  {
+    "revision": "d9a6e79582159d3cc1c55a7c04f46abe",
+    "url": "/portal/static/media/astma.d9a6e795.png"
+  },
+  {
+    "revision": "8b5717707f9fd2aa642caceba199cec6",
+    "url": "/portal/static/media/brzuch.8b571770.jpeg"
+  },
+  {
+    "revision": "509cfb95278e2e361b0bb8696522d361",
+    "url": "/portal/static/media/kostka.509cfb95.jpg"
+  },
+  {
+    "revision": "4ad33aa12a19da8f57f6f4e8202963a8",
+    "url": "/portal/static/media/przeziebienie.4ad33aa1.jpeg"
+  }
+]);
